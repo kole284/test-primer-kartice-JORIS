cd72168ec1ec4b3fd7195f776454a783e2edaaf7
kole284 <nikolampek04@gmail.com>
2025-10-08 10:20:50 +0200

    azuriran connectionString da radi bez docker kontejnera, lakse za ljude na windowsu

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c8947ffa3b6e35912c751997202d56c7ee866a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4148b74dc8a507adb848ab5620478cb8491361b")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
